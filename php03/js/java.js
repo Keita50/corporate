9fad757a87a03368e2418e6a591d80a20a7e0477
五十嵐啓太 <keita.igarashi555@gmail.com>
2019-11-24 18:11:15 +0900

    up

---
+++ php03/js/java.js
@@ -0,0 +1,26 @@
+$('.slider').slick({
+    autoplay:true,
+    autoplaySpeed:5000,
+    dots:true,
+    slidesToShow:4,
+    responsive:[
+        {
+            breakpoint: 1024,
+            settings:{
+                slidesToShow:3,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings:{
+                slidesToShow:2,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings:{
+                slidesToShow:1,
+            }
+        },
+    ]
+});
